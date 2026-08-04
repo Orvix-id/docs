f916769e9ac1104ee2b8370b3a6e994f36891daf
Eric Julianto <86970816+algonacci@users.noreply.github.com>
2026-08-04 15:16:43 +0700

    chore: remove GitHub button from navbar

--- src/lib/layout.shared.tsx
+++ src/lib/layout.shared.tsx
@@ -1,12 +1,11 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
-import { appName, gitConfig } from './shared';
+import { appName } from './shared';
 
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
       title: appName,
     },
-    githubUrl: `https://github.com/${gitConfig.user}/${gitConfig.repo}`,
     i18n: {
       defaultLanguage: 'en',
       languages: [
